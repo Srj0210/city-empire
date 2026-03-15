fb5b1ae226a67d265173af06b1326b574857dfad
Srj0210 <sleepyspider6166@gmail.com>
2026-03-15 13:20:08 +0530

    Add initial board configuration for game

---
+++ js/board.js
@@ -0,0 +1,15 @@
+export const board = [
+
+{name:"GO",type:"start"},
+{name:"Rajkot",type:"property",price:100,rent:10},
+{name:"Chance",type:"chance"},
+{name:"Ahmedabad",type:"property",price:120,rent:12},
+{name:"Tax",type:"tax",amount:50},
+
+{name:"Surat",type:"property",price:140,rent:14},
+{name:"Railway",type:"railway"},
+{name:"Chance",type:"chance"},
+{name:"Mumbai",type:"property",price:200,rent:20},
+{name:"Jail",type:"jail"}
+
+]
