4532678fc67c6b7eb0ccd31c6a34119f29c6e868
Srj0210 <sleepyspider6166@gmail.com>
2026-03-15 17:34:21 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,21 @@
 import { createBoard } from "./board.js";
+import { Player } from "./player.js";
+import { GameEngine } from "./engine.js";
 
 window.onload = () => {
 
-createBoard();
+  createBoard();
+
+  const board = document.getElementById("board");
+
+  const player = new Player();
+
+  player.spawn(board);
+
+  const engine = new GameEngine(player, board);
+
+  document.addEventListener("click", () => {
+    engine.roll();
+  });
 
 };
