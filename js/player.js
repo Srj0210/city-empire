e4767a648168769665f0c3b0c4aea29ab0fd7dbf
Srj0210 <sleepyspider6166@gmail.com>
2026-03-15 13:46:53 +0530

    Update player.js

--- js/player.js
+++ js/player.js
@@ -2,23 +2,26 @@ export class Player{
 
 constructor(name,color,type="human"){
 
-this.name = name
-this.color = color
-this.type = type
+this.name=name
+this.color=color
+this.type=type
 
-this.money = 1500
-this.position = 0
-this.properties = []
+this.money=1500
+this.position=0
+this.properties=[]
 
 }
 
 move(steps){
 
-this.position += steps
+this.position+=steps
 
-if(this.position >= 40){
-this.position -= 40
-this.money += 200
+if(this.position>=40){
+
+this.position-=40
+
+this.money+=200
+
 }
 
 }
