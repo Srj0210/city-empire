3d5b9fd1ca63f9f852ed45a9611ba5a91d422054
Srj0210 <sleepyspider6166@gmail.com>
2026-03-15 17:24:52 +0530

    Add Player class with movement and spawning methods

---
+++ js/player.js
@@ -0,0 +1,33 @@
+export class Player {
+
+constructor(){
+
+this.position = 0;
+
+this.token = document.createElement("div");
+
+this.token.classList.add("player");
+
+}
+
+spawn(board){
+
+board.children[this.position].appendChild(this.token);
+
+}
+
+move(steps, board){
+
+this.position += steps;
+
+if(this.position >= board.children.length){
+
+this.position = this.position % board.children.length;
+
+}
+
+board.children[this.position].appendChild(this.token);
+
+}
+
+}
