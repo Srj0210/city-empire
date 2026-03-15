a2b1d968db4085d21a0e16d5f856699fd6029d81
Srj0210 <sleepyspider6166@gmail.com>
2026-03-15 13:51:51 +0530

    Add initial board data for game

---
+++ js/data/boardData.js
@@ -0,0 +1,13 @@
+export const boardData=[
+
+{name:"GO",type:"start"},
+
+{name:"Rajkot",type:"property",price:100,rent:10},
+
+{name:"Chance",type:"chance"},
+
+{name:"Ahmedabad",type:"property",price:120,rent:12},
+
+{name:"Tax",type:"tax",amount:50}
+
+]
