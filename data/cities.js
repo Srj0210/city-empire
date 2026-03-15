68463ab558beb972ef6461136dfa8cefc95f5d15
Srj0210 <sleepyspider6166@gmail.com>
2026-03-15 17:20:55 +0530

    Add cities data to cities.js

---
+++ data/cities.js
@@ -0,0 +1,37 @@
+export const tiles = [
+
+"GO",
+
+"Rajkot",
+"Chance",
+"Ahmedabad",
+"Tax",
+"Railway",
+"Surat",
+"Chance",
+"Mumbai",
+
+"Jail",
+
+"Electric",
+"Bangalore",
+"Chance",
+"Chennai",
+"Railway",
+"Kolkata",
+"Chance",
+"Hyderabad",
+
+"Free Parking",
+
+"Pune",
+"Chance",
+"Jaipur",
+"Tax",
+"Lucknow",
+"Railway",
+"Chandigarh",
+"Chance",
+"Indore"
+
+];
